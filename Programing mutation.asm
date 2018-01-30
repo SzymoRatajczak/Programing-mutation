1df4a61f15135c64806bc0a033dcf3a3d717d0c9
Szymon Ratajczak <daniel.kruk1@interia.pl>
2018-01-30 02:55:01 +0100

    Add files via upload

---
+++ Programing mutation.asm
@@ -0,0 +1,51 @@
+evolution virus program 
+
+  sygnatura 1234567
+   subroutine infection_of_exe_file
+    begin
+      loop: take_random_ file
+	    if first_line_of_file==1234567
+		  then goto loop
+		 perform compression
+		 attach virus at the begining of the file
+    end
+	
+	subroutine destroy
+	 begin
+	 any type of acition you want
+	 end
+
+	 
+	subroutine launch_statement 
+	 begin 
+	   check  if launch statemnt is meet
+	 end
+	  
+	subroutine print_random_number
+    begin 
+      print random_value_name=random_value_name
+      loop:
+	     if random_bit=1
+		    print random_operator
+			goto loop
+		 print semicolon
+	end
+	
+	subroutine coping_virus
+	begin 
+	  loop:
+	    copy evolution virus 
+		 if random_bit=1 then
+		  print random_function
+		 if it is_not the_end of file then goto loop
+    end 
+	
+	
+  
+	  
+	begin 
+	   coping_virus
+	   infection_of_exe_file
+	   if launch_statement then destroy
+        
+    end		
